de20c12aae51e62c39260cd6a3861fc4898fc867
Rémy Groleau <remy.groleau@hotmail.fr>
2016-03-01 14:59:51 -0500

    new page
    new page

---
+++ front-page.php
@@ -0,0 +1,45 @@
+<?php
+
+get_header();
+
+?>
+
+
+<section class="firstSlide">
+<?php
+the_post_thumbnail('full')
+?>
+</section>
+
+
+<section class="asideSlide1">
+    
+    <article class="artBvn">
+        
+        <h2>
+            
+            Bienvenue
+            
+        </h2>
+        
+    </article>
+    
+    
+    <article class="artActu">
+        
+        <h2>
+            
+            Actualités
+            
+        </h2>
+        
+    </article>
+    
+</section>
+
+
+<?php
+
+get_footer();
+
+?>
