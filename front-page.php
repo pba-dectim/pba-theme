9c41e2110fb4fbd99253fe3deb772d086a039467
Rémy Groleau <remy.groleau@hotmail.fr>
2016-03-03 10:12:48 -0500

    new styles remy
    new styles remy

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@ get_header();
 
 
 <section class="firstSlide">
-<video class="full_video" id="js-video" src="video/accueil.mp4" autoplay="" loop="" muted="" type="video/mp4" poster="http://www.ville.candiac.qc.ca/modules/candiac/assets/images/home-background.jpg"></video>
+<video id="full_video" id="js-video" src="<?php echo get_stylesheet_directory_uri(); ?>/video/accueil.mp4" autoplay="" loop="" muted="" type="video/mp4" poster="http://www.ville.candiac.qc.ca/modules/candiac/assets/images/home-background.jpg"></video>
 </section>
 
 
@@ -20,16 +20,29 @@ get_header();
             
         </h2>
         
+        
+                <p>
+            
+            Quisque consequat massa ut lacinia interdum. Curabitur a sapien est. Morbi semper iaculis augue sed rhoncus. Integer in arcu et ipsum lacinia pellentesque. In hac habitasse platea dictumst. Mauris nec imperdiet tortor. Cras vel mattis tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer convallis nec libero in accumsan.
+            
+        </p>
+        
     </article>
     
     
-    <article class="artActu">
+    <article class="artBvn">
         
         <h2>
             
             Actualités
             
         </h2>
+        
+        <p>
+            
+            Quisque consequat massa ut lacinia interdum. Curabitur a sapien est. Morbi semper iaculis augue sed rhoncus. Integer in arcu et ipsum lacinia pellentesque. In hac habitasse platea dictumst. Mauris nec imperdiet tortor. Cras vel mattis tortor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer convallis nec libero in accumsan.
+            
+        </p>
         
     </article>
     
