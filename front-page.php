f6e6d4c0b206d41c823b5f2889e63e4ea71cf378
Rémy Groleau <remy.groleau@hotmail.fr>
2016-03-03 13:09:45 -0500

    new css 13h09
    new css 13h09

--- front-page.php
+++ front-page.php
@@ -5,10 +5,12 @@ get_header();
 ?>
 
 
-<section class="firstSlide">
-<video id="full_video" id="js-video" src="<?php echo get_stylesheet_directory_uri(); ?>/video/accueil.mp4" autoplay="" loop="" muted="" type="video/mp4" poster="http://www.ville.candiac.qc.ca/modules/candiac/assets/images/home-background.jpg"></video>
-</section>
-
+<figure class="firstSlide">
+          <video id="full_video" id="js-video" src="<?php echo get_stylesheet_directory_uri(); ?>/video/accueil.mp4" autoplay="" loop="" muted="" type="video/mp4" poster="http://www.ville.candiac.qc.ca/modules/candiac/assets/images/home-background.jpg"></video> 
+            <figcaption>
+                <h1>Le patrimoine</h1>
+                <h2>Pour que l'avenir se souvienne !</h2></figcaption>
+        </figure>
 
 <section class="asideSlide1">
     
@@ -45,6 +47,52 @@ get_header();
         </p>
         
     </article>
+    
+</section> 
+
+<section class="secMandat">
+    
+    <h1>Notre Mandat</h1>
+    
+<div class="wrapMandat">    
+    <article>
+    <p>
+        
+        La Société du patrimoine de Boucherville, organisme à but non lucratif, a comme objectif de promouvoir les intérêts socio-culturels et patrimoniaux des citoyens et des organismes de Boucherville. Pour y parvenir, elle privilégie les moyens suivants:
+        
+    </p>
+    
+    <ul>
+        
+        <li>conception de projets d'écomusée</li>
+        <li>organisation de conférences, expositions, événements spéciaux</li>
+        <li>publication de documents</li>
+        <li>campagnes de levée de fonds et organisation d'activités de financement</li>
+        
+    </ul>
+    
+    </article>
+    
+    <article>
+        
+        
+        <p>
+            La Société est ouverte sans restriction à tout citoyen ou organisme intéressé au patrimoine de Boucherville, que ce soit dans ses aspects historique, culturel, architectural, religieux ou environnemental, et désireux de participer à sa mise en valeur.
+            
+        </p>
+        
+        
+        
+        <p>
+            
+            Pour devenir membre, il suffit de payer une cotisation annuelle de 10$ (individu), de 25$ (organisme à but non lucratif), ou de 100$ (société commerciale). La Société du patrimoine a, de plus, reçu le mandat du conseil municipal de créer et d'exploiter des écomusées.
+            
+        </p>
+        
+        
+    </article>
+    
+    </div>
     
 </section>
 
