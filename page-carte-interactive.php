9f433fdd468909ce1ef1b05be535829a9d255c86
Félix-Antoine Gosselin <fagosselin@hotmail.ca>
2016-03-08 14:53:03 -0500

    Enlever le titre page carte intéractive
    Enlever le titre page carte intéractive

--- page-carte-interactive.php
+++ page-carte-interactive.php
@@ -22,12 +22,9 @@ get_header(); ?>
 				while ( have_posts() ) : the_post();
 
 					// Include the page content template.
-					get_template_part( 'content', 'page' );
+					?><!--<h1><?php the_title();?></h1>--><?php
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
+					
 				endwhile;
 			?>
 			
