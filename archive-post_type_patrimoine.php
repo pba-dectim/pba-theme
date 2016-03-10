e715b6f6d4d66a46c8f39324b5cfe607870acaa7
jf-airedejeu <jfdube1992@gmail.com>
2016-03-10 12:15:28 -0500

    Ajustemkent function et listing
    Ajustemkent function et listing jf-10-mars

--- archive-post_type_patrimoine.php
+++ archive-post_type_patrimoine.php
@@ -7,89 +7,69 @@ get_header(); ?>
 		<div id="content" class="site-content" role="main">
         
 
-			<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) : ?>
 
-			 <section class='banner-titre' style='background-image:url("<?php get_template_directory_uri(); ?>"/img/46-pierre-boucher-arriere.jpg); '>
+			 <section class='banner-titre' style='background-image:url("<?php echo get_stylesheet_directory_uri(); ?>/img/46-pierre-boucher-arriere.jpg"); '>
                         <div class='banner_filter'></div>
-                        <h1>Le Patrimoine</h1>
+                        <h1>Le patrimoine</h1>
               </section>
-              	<div>
-				
-  <?php $args = array(
-	'show_option_all'    => '',
-	'show_option_none'   => 'Veuillez choisir une catégorie à afficher',
-	'option_none_value'  => '-1',
-	'orderby'            => 'name', 
-	'order'              => 'ASC',
-	'show_count'         => 0,
-	'hide_empty'         => 0, 
-	'child_of'           => 0,
-	'exclude'            => '',
-	'echo'               => 1,
-	'selected'           => 0,
-	'hierarchical'       => 1, 
-	'name'               => 'cat',
-	'id'                 => '',
-	'class'              => 'postform',
-	'depth'              => 0,
-	'tab_index'          => 0,
-	'taxonomy'           => 'patrimoine_category',
-	'hide_if_empty'      => false,
-	'value_field'	     => 'term_id',	
-); ?>
-<?php wp_dropdown_categories( $args ); ?>   
-           
-                      <div id="ficheInfo" class="blockInfo">
-
-                                <h3>CATÉGORIE(S)</h3>
-                                <?php 
-                               
-                                $term = get_field('categorie_de_lelement');
-                               
-                                if( $term ): ?>
-
-                                    <p><?php echo $term->name; ?><p>
-                                    
-                                   
-                                <?php endif; ?>
-
-
-                                <h3>ANNÉE DE CONSTRUCTION</h3>
-                                <p><?php the_field('annee_de_construction'); ?></p>
-
-                                <h3>PRIX</h3>
-                                <?php the_field('prix_ou_nomination'); ?>
-
-                                <h3>ADRESSE</h3>
-                                <p><?php the_field('adresse_patrimoine'); ?></p>
-
-                            </div>
-			
-				<section id="listing-content-ajax">
-			<?php
-					// Start the Loop.
-					while ( have_posts() ) : the_post(); 
-
-					$post_id = get_the_ID();
-			?>
-                    <article class="listing-post-single">
-						<h1><?php the_field('date_de_la_realisation', $post_id);?><span>-</span><?php the_title(); ?></h1>
-						<?php /*?>get_template_part( 'content', get_post_format() );<?php */?>
-						<p><?php the_excerpt(); ?></p>
-                        <img src="<?php the_post_thumbnail_url('medium'); ?>" alt="test" />
-                        <a href="<?php the_permalink(); ?>">Voir la fiche</a>
-					</article>
-					<?php endwhile; ?>
-				</section>
+              <div>
+               	
+               <section id="listing-content-ajax">
+                    <div id="ficheInfo" class="blockInfo">
+                        <h3>CATÉGORIE(S)</h3>
+                        <p><?php echo category_description( $category_id ); ?></p>
+                    </div> 
+                    <div>
+                        <?php $args = array(
+                            'show_option_all'    => '',
+                            'show_option_none'   => 'Veuillez choisir une catégorie à afficher',
+                            'option_none_value'  => '-1',
+                            'orderby'            => 'name', 
+                            'order'              => 'ASC',
+                            'show_count'         => 0,
+                            'hide_empty'         => 0, 
+                            'child_of'           => 0,
+                            'exclude'            => '',
+                            'echo'               => 1,
+                            'selected'           => 0,
+                            'hierarchical'       => 1, 
+                            'name'               => 'cat-selector',
+                            'id'                 => 'cat',
+                            'class'              => 'postform',
+                            'depth'              => 0,
+                            'tab_index'          => 0,
+                            'taxonomy'           => 'patrimoine_category',
+                            'hide_if_empty'      => false,
+                            'value_field'	     => 'term_id',	
+                        ); ?>
+                        <?php wp_dropdown_categories( $args ); ?>           
+                        <?php
+                            while ( have_posts() ) : the_post(); 
+                            
+                        ?>
+                         <article class="listing-post-single">
+                             <div>
+                              <h1><?php the_title(); ?></h1>
+                              <p><?php the_excerpt(); ?></p>
+                              <a href="<?php the_permalink(); ?>">Voir la fiche</a>
+                             </div>
+                             <div>
+                              <?php the_post_thumbnail('medium'); ?>
+                             </div>
+                        </article>
+                        
+                            <?php endwhile; ?>
+                     </div>
+			    </section>
 					<?php // Previous/next page navigation.
 					twentyfourteen_paging_nav(); ?>
 
-				<?php endif;
+		<?php endif;?>
 		
-			?>
-            </div>
-		</div><!-- #content -->
-	</section><!-- #primary -->
 
+          </div>
+	  </div><!-- #content -->
+	</section><!-- #primary -->
 <?php
 get_footer();
